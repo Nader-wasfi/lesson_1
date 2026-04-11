ff151ed911195a836076769319fcbe53ea396646
Nader Wasfi <naderwasfi94@gmail.com>
2026-04-11 13:53:53 +0200

    Update tasks comments

--- bin/task3.dart
+++ bin/task3.dart
@@ -1,5 +1,5 @@
 void main() {
- 
+ // the const number is 7 
 const num =7;
 print(num);
 }
--- bin/task4.dart
+++ bin/task4.dart
@@ -1,8 +1,9 @@
 void main() {
- 
+ // Definition Variables 
  double p = 1328.32;
  double t = 3.3;
  double r = 7;
+ // Apply the Formula
  double simple_interest = ( p * t * r ) / 100;
  print(simple_interest);
 
