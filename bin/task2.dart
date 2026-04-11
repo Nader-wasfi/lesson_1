14fa243e4d68f40758247148d61d7af477ed4b9f
Nader Wasfi <naderwasfi@Naders-MacBook-Air.local>
2026-04-11 13:24:10 +0200

    first

---
+++ bin/task2.dart
@@ -0,0 +1,5 @@
+void main() {
+ 
+  print("Hello I am 'John Doe' \nHello I'm “John Doe”");
+
+}
---
+++ bin/task4.dart
@@ -0,0 +1,9 @@
+void main() {
+ 
+ double p = 1328.32;
+ double t = 3.3;
+ double r = 7;
+ double simple_interest = ( p * t * r ) / 100;
+ print(simple_interest);
+
+}
